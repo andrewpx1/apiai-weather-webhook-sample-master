79f8a155cd66cc78fa3595a7b1bc52f0848cee18
andrewpx1 <30318211+andrewpx1@users.noreply.github.com>
2017-07-21 01:33:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,15 +36,56 @@ def webhook():
 
 
 def processRequest(req):
-   if req.get("result").get("action") != "getjoke":
+    if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-        baseurl = "http://api.icndb.com/jokes/random"
-        result = urllib.urlopen(baseurl).read()
-        data = json.loads(result)
-    valueString = data.get('value')
-    joke = valueString.get('joke')
-    speech = joke
-    displayText = joke    
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "🐇🐇Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
@@ -57,7 +98,7 @@ def processRequest(req):
         "source": "apiai-weather-webhook-sample"
     }
 
-   
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
