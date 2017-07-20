7778965648fe851e61b70b5f345f97a2c7457eca
andrewpx1 <30318211+andrewpx1@users.noreply.github.com>
2017-07-21 02:13:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,21 +36,20 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") == "yahooWeatherForecast":
- 
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+	if req.get("result").get("action") == "yahooWeatherForecast":
+		baseurl = "https://query.yahooapis.com/v1/public/yql?"
+		yql_query = makeYqlQuery(req)
+		if yql_query is None:
+		   return {}
+		yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+		result = urlopen(yql_url).read()
+		data = json.loads(result)
+		res = makeWebhookResult(data)
 	elif req.get("result").get("action") == "getjoke":
-        baseurl = "http://api.icndb.com/jokes/random"
-        result = urlopen(baseurl).read()
-        data = json.loads(result)
-        res = makeWebhookResultForGetJoke(data)
+		baseurl = "http://api.icndb.com/jokes/random"
+		result = urlopen(baseurl).read()
+		data = json.loads(result)
+		res = makeWebhookResultForGetJoke(data)
     else:
         return {}
     return res
@@ -107,11 +106,11 @@ def makeWebhookResult(data):
 
 
 def makeWebhookResultForGetJoke(data):
-    valueString = data.get('value')
-    joke = valueString.get('joke')
-    speechText = joke
-    displayText = joke
-    return {
+	valueString = data.get('value')
+	joke = valueString.get('joke')
+	speechText = joke
+	displayText = joke
+	return {
         "speech": speechText,
         "displayText": displayText,
         # "data": data,
