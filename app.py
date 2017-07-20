18a00db85414271ef857de090ffba4df23055b0b
andrewpx1 <30318211+andrewpx1@users.noreply.github.com>
2017-07-21 01:42:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
+    if req.get("result").get("action") == "yahooWeatherForecast":
+ 
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
@@ -46,6 +46,13 @@ def processRequest(req):
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
+	elif req.get("result").get("action")=="getjoke":
+        baseurl = "http://api.icndb.com/jokes/random"
+        result = urlopen(baseurl).read()
+        data = json.loads(result)
+        res = makeWebhookResultForGetJoke(data)
+    else:
+        return {}
     return res
 
 
@@ -99,6 +106,20 @@ def makeWebhookResult(data):
     }
 
 
+def makeWebhookResultForGetJoke(data):
+    valueString = data.get('value')
+    joke = valueString.get('joke')
+    speechText = joke
+    displayText = joke
+    return {
+        "speech": speechText,
+        "displayText": displayText,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+	
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
