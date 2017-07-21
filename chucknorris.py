c6669e6131da6d597ffae5377af1681f38c064b8
andrewpx1 <30318211+andrewpx1@users.noreply.github.com>
2017-07-21 14:53:50 +0530

    Update chucknorris.py

--- chucknorris.py
+++ chucknorris.py
@@ -19,25 +19,7 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-
 def processRequest(req):
-    if req.get("result").get("action") != "getjoke":
-        return {}
     baseurl = "http://api.icndb.com/jokes/random"
     result = urlopen(baseurl).read()
     data = json.loads(result)
