3c893801f2db209bcdf70328def77eaaa7265cd8
andrewpx1 <30318211+andrewpx1@users.noreply.github.com>
2017-07-24 16:31:34 +0530

    Update pxbun.py

--- pxbun.py
+++ pxbun.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 from os.path import splitext
+
 import urllib
 import json
 import os
