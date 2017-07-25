981771a80e8fe5a3dc8879bb7881ed2188b25483
andrewpx1 <30318211+andrewpx1@users.noreply.github.com>
2017-07-25 18:51:47 +0530

    Add files via upload

---
+++ pxdad.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+def processRequest(req):
+	cmd = 'curl https://icanhazdadjoke.com/'
+	hdi = os.system(cmd)
+	hfi = str(hdi)
+	spli = hfi.split("0")
+    joke = spli[0]
+
+	# print(json.dumps(item, indent=4))
+	
+    speech = joke
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
