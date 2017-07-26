92abf8e521fc69a1f8a5a3b552cbec9e661e5c6a
andrewpx1 <30318211+andrewpx1@users.noreply.github.com>
2017-07-26 17:40:50 +0530

    Add files via upload

---
+++ yodapx.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from os.path import splitext
+
+import json
+import os
+import requests
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+def processRequest(req):
+	headers = {
+		"X-Mashape-Key": "axWE0J6Hj5mshIyeWhO19vjpSYyxp1k53ohjsnr3rxp4xsIj8I",
+		"Accept": "text/plain"
+	}
+	r = requests.get('https://yoda.p.mashape.com/yoda?sentence=wait+for+me', headers=headers)
+	joke = r.text
+	speech = joke
+	
+	print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
