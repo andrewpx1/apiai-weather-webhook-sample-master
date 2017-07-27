8cfea00b0838bfe10e0b691ef0ca3019b3f03f24
andrewpx1 <30318211+andrewpx1@users.noreply.github.com>
2017-07-28 01:16:23 +0530

    Update pxmeme.py

--- pxmeme.py
+++ pxmeme.py
@@ -22,25 +22,25 @@ app = Flask(__name__)
 
 
 def processRequest(req):
-	result = req.get("result")
-	parameters = result.get("parameters")
-	prt = parameters.get("any")
-	baseurl = "http://www.memes.com/search/"
-	mydict = {'': prt}
-	put = urlencode(mydict)
-	num1 = randint(11, 12) - 10
-	num2 = randint(0, 8)
-	url = baseurl + put + '/' + str(num1)
-	hj = urlopen(url)
-	gh = hj.read()
-	cbt = gh.decode()
-	dbt = 'id="image_(.+?)"'
-	ebt = re.findall(dbt,cbt)
-	fbt = ebt[num2]
-	xurl = "http://images.memes.com/meme/"
-	poke = xurl + fbt
-	toke = '{"file":"' + poke + '"}'
-	data = json.loads(toke)
+    result = req.get("result")
+    parameters = result.get("parameters")
+    prt = parameters.get("any")
+    baseurl = "http://www.memes.com/search/"
+    mydict = {'': prt}
+    put = urlencode(mydict)
+    num1 = randint(11, 12) - 10
+    num2 = randint(0, 8)
+    url = baseurl + put + '/' + str(num1)
+    hj = urlopen(url)
+    gh = hj.read()
+    cbt = gh.decode()
+    dbt = 'id="image_(.+?)"'
+    ebt = re.findall(dbt,cbt)
+    fbt = ebt[num2]
+    xurl = "http://images.memes.com/meme/"
+    poke = xurl + fbt
+    toke = '{"file":"' + poke + '"}'
+    data = json.loads(toke)
     res = makeWebhookResult(data)
     return res
 	
